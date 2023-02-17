4fc86101e4d070f88954636b6f32cb2b0996cca5
Milad <milad.mosavi.developer@gmail.com>
2023-02-17 13:35:22 +0330

    generate new report

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-## Technical Analysis Report
+Financial Report
 
-Here is a summary of the technical analysis indicators for BTC-USD over the past 24 hours:
+Market Status:
+Based on the following indicators:
 
-| Indicator | Trend | Sentiment |
-| --- | --- | --- |
-| Bollinger Bands | Neutral | Positive |
-| Fibonacci Retracement Levels | Bullish | Positive |
-| Moving Average | Bullish | Positive |
-| MACD | Bearish | Positive |
-| OBV | Bullish | Negative |
-| RSI | Bullish | Positive |
-| Stochastic Oscillator | Bullish | Very Positive |
-| Williams %R | Bearish | Positive |
+                                Trend      Sentiment
+Bollinger Bands               Neutral       Positive
+Fibonacci Retracement Levels  Bullish       Positive
+Moving Average                Bullish       Positive
+MACD                          Bearish       Positive
+OBV                           Bullish       Negative
+RSI                           Bullish       Positive
+Stochastic Oscillator         Bullish  Very Positive
+Williams %R                   Bearish       Positive
 
-Overall, BTC-USD has exhibited a mix of bullish and bearish indicators in the past 24 hours. While some indicators suggest positive momentum, others indicate a more neutral or negative outlook. Investors should exercise caution and carefully consider all available information before making any trading decisions.
+Summary:
+The market is currently Bullish with a slightly mixed sentiment.
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import RSI
 import StochasticOscillator
 import WilliamsR
 import os
+import pandas as pd
 
 def save(report):
     with open("README.md", "a") as file:
@@ -52,21 +53,56 @@ wr_trend, wr_sentiment = wr.generate_report()
 # ichimokuCloudReport = ichimokuCloud.calculate()
 # save(ichimokuCloudReport)
 
+# create a dictionary to store the trend and sentiment data for each indicator
+indicators = {
+    'Bollinger Bands': [bollingerBands_trend, bollingerBands_sentiment],
+    'Fibonacci Retracement Levels': [fibonacci_trend, fibonacci_sentiment],
+    'Moving Average': [moving_average_trend, moving_average_sentiment],
+    'MACD': [macd_trend, macd_sentiment],
+    'OBV': [obv_trend, obv_sentiment],
+    'RSI': [rsi_trend, rsi_sentiment],
+    'Stochastic Oscillator': [stoch_trend, stoch_sentiment],
+    'Williams %R': [wr_trend, wr_sentiment]
+}
+
+# create a pandas dataframe from the dictionary
+df = pd.DataFrame.from_dict(indicators, orient='index', columns=['Trend', 'Sentiment'])
+
+# create a function to generate the financial report
+def generate_report():
+    report = 'Financial Report\n\n'
+    report += 'Market Status:\n'
+    report += 'Based on the following indicators:\n\n'
+    report += df.to_string() + '\n\n'
+    report += 'Summary:\n'
+    report += 'The market is currently '
+    
+    # determine the overall trend and sentiment based on the indicators
+    bullish_count = df['Trend'].str.contains('Bullish').sum()
+    bearish_count = df['Trend'].str.contains('Bearish').sum()
+    neutral_count = df['Trend'].str.contains('Neutral').sum()
+    
+    if bullish_count > bearish_count:
+        report += 'Bullish'
+    elif bearish_count > bullish_count:
+        report += 'Bearish'
+    else:
+        report += 'Neutral'
+    
+    report += ' with a '
+    
+    if neutral_count > 0:
+        report += 'slightly '
+    
+    if all(df['Sentiment'] == 'Positive'):
+        report += 'positive sentiment.'
+    elif all(df['Sentiment'] == 'Negative'):
+        report += 'negative sentiment.'
+    else:
+        report += 'mixed sentiment.'
+    
+    return report
+
 
 deleteReadMe()
-save(bollingerBands_trend+ "\n")
-save(bollingerBands_sentiment+ "\n")
-save(fibonacci_trend+ "\n")
-save(fibonacci_sentiment+ "\n")
-save(moving_average_trend+ "\n")
-save(moving_average_sentiment+ "\n")
-save(macd_trend+ "\n")
-save(macd_sentiment+ "\n")
-save(obv_trend+ "\n")
-save(obv_sentiment+ "\n")
-save(rsi_trend+ "\n")
-save(rsi_sentiment+ "\n")
-save(stoch_trend+ "\n")
-save(stoch_sentiment+ "\n")
-save(wr_trend+ "\n")
-save(wr_sentiment+ "\n")
+save(generate_report())
